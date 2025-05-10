c28df49dc79f34f27b2f0256263739ad48fe4523
Boris-Andrei Pavel <boris@Boris-Andreis-MacBook-Air.local>
2025-05-11 00:17:55 +0300

    Refactor quote and author elements in HTML and update quote generation logic in JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const quoteContainer = document.getElementById('quote-container');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const xBtn = document.getElementById('x-social');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = []
 
 // Show new quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    return quote;
+    const quoteRandom = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    author.textContent = quoteRandom.author || 'Unknown';
+    quote.textContent = quoteRandom.text;
 }
 
 // Get quotes from the API
